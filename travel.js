f45417450d899e68be2886295f0b8b9aa0270df7
Ben Corn <bcorn@bu.edu>
2021-07-18 16:14:09 -0400

    Adding travel log

---
+++ travel.js
@@ -0,0 +1,22 @@
+// Initialize and add the map
+function initMap() {
+  const montreal = { lat: 45.50601149849052, lng: -73.56423816445884 };
+  const quebec = {lat: 46.8175818361284, lng: -71.17439726042728};
+  const vancouver = {lat: 49.28292478826721, lng: -123.12215850697119}
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: montreal,
+  });
+  const marker1 = new google.maps.Marker({
+    position: montreal,
+    map: map,
+  });
+  const marker2 = new google.maps.Marker({
+    position: quebec,
+    map: map,
+  });
+  const marker3 = new google.maps.Marker({
+    position: vancouver,
+    map: map,
+  });
+}
