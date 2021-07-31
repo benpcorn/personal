2052a0c57fedef50bdb6f27164f11421c8a6d863
Ben Corn <bcorn@bu.edu>
2021-07-31 11:08:12 -0400

    Special surprise

--- travel.js
+++ travel.js
@@ -1,8 +1,25 @@
 // Initialize and add the map
+fart = new Fart({
+  default_sound: farts.toot,
+  loop: false,
+  volume: 100
+});
+
+
+$(document).ready(function () {
+  document.body.addEventListener("mousemove", function () {
+    fart.play();
+  }, { once: true });
+  document.body.addEventListener("click", function () {
+    fart.play();
+  }, { once: true });
+});
+
+
 function initMap() {
   const montreal = { lat: 45.50601149849052, lng: -73.56423816445884 };
-  const quebec = {lat: 46.8175818361284, lng: -71.17439726042728};
-  const vancouver = {lat: 49.28292478826721, lng: -123.12215850697119}
+  const quebec = { lat: 46.8175818361284, lng: -71.17439726042728 };
+  const vancouver = { lat: 49.28292478826721, lng: -123.12215850697119 }
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
     center: montreal,
